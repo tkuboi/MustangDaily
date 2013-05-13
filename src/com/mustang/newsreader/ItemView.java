6f4ea3b2bfe898c3512c87b8c3bc887e51ac1bd0
Chun Hui Pek <cpek@calpoly.edu>
2013-05-13 10:50:21 -0700

    WebView
    Add WebView programmatically

--- src/com/mustang/newsreader/ItemView.java
+++ src/com/mustang/newsreader/ItemView.java
@@ -7,24 +7,21 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class ItemView extends LinearLayout {
+    private Context context;
     private Article m_article;
-//    private WebView m_vwFirstImage;
-//	  private TextView m_vwFirstTitleText;
-//	  private TextView m_vwFirstDescText;
+    private WebView m_vwImage;
     private TextView m_vwTitleText;
     private TextView m_vwDescText;
 
 	public ItemView(Context context) {
 		super(context);
+		this.context = context;
 	}
 	
 	public ItemView(Context context, Article item) {
 		super(context);
 	    LayoutInflater inflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 	    inflater.inflate(R.layout.item_view, this, true);
-//	    this.m_vwFirstImage = (WebView) findViewById(R.id.first_imageView);
-//	    this.m_vwFirstTitleText = (TextView) findViewById(R.id.first_titleView);
-//	    this.m_vwFirstDescText = (TextView) findViewById(R.id.first_descView);
         this.m_vwTitleText = (TextView) findViewById(R.id.titleView);
         this.m_vwDescText = (TextView) findViewById(R.id.descView);
 	    setItem(item);
@@ -33,16 +30,12 @@ public class ItemView extends LinearLayout {
 	public void setItem(Article item) {
 		m_article = item;
 		
-//        if(m_article.getImgsrc() != null && m_article.getImgsrc().size() > 0) {
-//            String imgSrc = m_article.getImgsrcAt(0);
-//            this.m_vwFirstImage.loadUrl(imgSrc);
-//            this.m_vwFirstTitleText.setText(m_article.getTitle());
-//            this.m_vwFirstDescText.setText(m_article.getDescription());
-//        }
-//        else {
-//    	    this.m_vwTitleText.setText(m_article.getTitle());
-//    	    this.m_vwDescText.setText(m_article.getDescription());
-//        }
+        if(m_article.getImgsrc() != null && m_article.getImgsrc().size() > 0) {
+            String imgSrc = m_article.getImgsrcAt(0);
+            m_vwImage = new WebView(context);
+            this.addView(m_vwImage);
+            this.m_vwImage.loadUrl(imgSrc);
+        }
 		
 		this.m_vwTitleText.setText(m_article.getTitle());
         this.m_vwDescText.setText(m_article.getDescription());
